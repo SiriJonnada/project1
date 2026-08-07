85fb6a8738188fe82c525f512fc0a833e48f14f2
SiriJonnada <sirijonnada07@gmail.com>
2026-08-07 18:47:48 -0500

    created fast API for project.

--- ClinicalTrials_Project1.py
+++ ClinicalTrials_Project1.py
@@ -1,47 +1,51 @@
-import requests # type: ignore
+import requests 
 import json
 from collections import Counter 
 import matplotlib.pyplot as plt
 
-## Should put in an if then statement in place of line 9 to get information based on schizophrenia or atreal thing
-prompt_condition = input("Which medical condition would you like to search for?")
-condition_name = prompt_condition
+def get_studies(condition):
+    
+    condition_name = condition
 
-params = {
-    "format" : "json",
-    "query.cond" : condition_name
-}
+    params = {
+        "format" : "json",
+        "query.cond" : condition_name
+    }
 
-r = requests.get('https://clinicaltrials.gov/api/v2/studies', params = params)
+    r = requests.get('https://clinicaltrials.gov/api/v2/studies', params = params)
 
-data = r.json()
+    data = r.json()
 
-with open ("clinicaltrials.json", "w") as f:
-    json.dump(data, f, indent = 4)
+    with open ("clinicaltrials.json", "w") as f:
+        json.dump(data, f, indent = 4)
 
-## Statistic could be max age
+    return data
 
-maximum_ages = []
+    ## Statistic could be max age
+def get_maximum_ages(data, condition):
+    maximum_ages = []
 
-for study in data["studies"]:
-    age = study["protocolSection"]["eligibilityModule"].get("maximumAge")
-    if age is not None:
-        maximum_ages.append(age)
+    for study in data["studies"]:
+        age = study["protocolSection"]["eligibilityModule"].get("maximumAge")
+        if age is not None:
+            maximum_ages.append(age)
 
-ages_data = []
+    ages_data = []
 
-for age in maximum_ages:
-        number = age.replace("Years", "")
-        number = int(number)
-        ages_data.append(number)
+    for age in maximum_ages:
+            number = age.replace("Years", "")
+            number = int(number)
+            ages_data.append(number)
 
-measure_ages = Counter(ages_data)
+    measure_ages = Counter(ages_data)
 
-unique_ages = list(measure_ages.keys())
-count_ages = list(measure_ages.values())
+    unique_ages = list(measure_ages.keys())
+    count_ages = list(measure_ages.values())
 
-plt.bar(unique_ages, count_ages)
-plt.title(f"Maximum ages in {condition_name} studies")
-plt.xlabel("Maximum Age (Years)")
-plt.ylabel("Number of Studies")
-plt.show()
+    plt.bar(unique_ages, count_ages)
+    plt.title(f"Maximum ages in {condition} studies")
+    plt.xlabel("Maximum Age (Years)")
+    plt.ylabel("Number of Studies")
+    plt.show()
+
+    return unique_ages, count_ages
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# project1
-clinical trials
+# ClinicalTrialsAPI-Project
+Purpose: Exctract data from ClinicalTrials.gov
+Outcome: Queries the user on what medical condition they want information for, then creates a JSON file with the first 10 studies for that result. Then, it pulls the maximum age data to create a bar graph displaying the distribution of maximum age limits across the first 10 clinical trials.
